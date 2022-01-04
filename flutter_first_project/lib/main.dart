0c3f6af3ff25c969488c54f4621ce4057248fc6a
kubrasambur <kubrasambur123@gmail.com>
2022-01-04 15:55:52 +0100

    04.01.22 homework

--- flutter_first_project/lib/main.dart
+++ flutter_first_project/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, deprecated_member_use, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -8,6 +8,155 @@ void main() {
 
 // ignore: must_be_immutable
 class MyApp extends StatelessWidget {
+  Row dartRowunuOlustur() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade200,
+          child: Text(
+            "D",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade400,
+          child: Text(
+            "A",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade600,
+          child: Text(
+            "R",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade800,
+          child: Text(
+            "T",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Column dersleriColumnuOlustur() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          margin: EdgeInsets.only(top: 15),
+          color: Colors.orange.shade200,
+          child: Text(
+            "E",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade300,
+          child: Text(
+            "R",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade400,
+          child: Text(
+            "S",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade500,
+          child: Text(
+            "L",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade600,
+          child: Text(
+            "E",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade700,
+          child: Text(
+            "R",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.orange.shade800,
+          child: Text(
+            "İ",
+            style: TextStyle(
+              fontSize: 55,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   // ignore: prefer_final_fields
   String _img1 =
       "https://media.istockphoto.com/photos/moraine-lake-rocky-mountains-canada-picture-id525508231?b=1&k=20&m=525508231&s=170667a&w=0&h=AgqcnKG6W8tsIepho7lvZM6iGQfqUCFTgrF_U5a0l5Y=";
@@ -18,53 +167,20 @@ class MyApp extends StatelessWidget {
           ThemeData(primaryColor: Colors.red, accentColor: Colors.indigoAccent),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Başlık"),
+          title: Text("Flutter Dersleri"),
           backgroundColor: Colors.amber[900],
         ),
         backgroundColor: Colors.indigo[100],
         body: Container(
-            color: Colors.purple[200],
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  //aşağıdaki satırı yazmazsak ya da max yaparsak Rowda da yayılacağı  için bütün ekran kullanılacak
-                  mainAxisSize: MainAxisSize.min,
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    Text("K"),
-                    Text("U"),
-                    Text("B"),
-                    Text("R"),
-                    Text("A"),
-                  ],
-                ),
-                Icon(
-                  Icons.add_circle,
-                  size: 64,
-                  color: Colors.green,
-                ),
-                Icon(
-                  Icons.add_circle,
-                  size: 64,
-                  color: Colors.orange,
-                ),
-                Icon(
-                  Icons.add_circle,
-                  size: 64,
-                  color: Colors.blue,
-                ),
-                Icon(
-                  Icons.add_circle,
-                  size: 64,
-                  color: Colors.red,
-                ),
-              ],
-            )),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              dartRowunuOlustur(),
+              Expanded(child: dersleriColumnuOlustur())
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             debugPrint("tıklandı");
@@ -74,6 +190,111 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  List<Widget> get sorunluContainer {
+    return [
+      Container(
+        width: 75,
+        height: 75,
+        color: Colors.yellow,
+      ),
+      Container(
+        width: 75,
+        height: 75,
+        color: Colors.red,
+      ),
+      Container(
+        width: 75,
+        height: 75,
+        color: Colors.blue,
+      ),
+      Container(
+        width: 75,
+        height: 75,
+        color: Colors.orange,
+      ),
+      Container(
+        width: 75,
+        height: 75,
+        color: Colors.blue,
+      ),
+      Container(
+        width: 75,
+        height: 75,
+        color: Colors.red,
+      ),
+    ];
+  }
+
+  List<Widget> get expandedContainer {
+    return [
+      Expanded(
+        //flex değeri vererek bu kutunun diğerlerinden fazla alan kaplamasını sağlayabiliriz
+        flex: 2,
+        child: Container(
+          //artık bu değerlerin bi önemi kalmadı çünkü expanded kullanıyoruz yani dahip olunan alana hepsini sığdırıyoruz.
+          width: 75,
+          height: 75,
+          color: Colors.yellow,
+        ),
+      ),
+      Expanded(
+        child: Container(
+          width: 75,
+          height: 75,
+          color: Colors.red,
+        ),
+      ),
+      Expanded(
+        child: Container(
+          width: 75,
+          height: 75,
+          color: Colors.blue,
+        ),
+      ),
+      Expanded(
+        child: Container(
+          width: 75,
+          height: 75,
+          color: Colors.orange,
+        ),
+      ),
+      Expanded(
+        child: Container(
+          width: 75,
+          height: 75,
+          color: Colors.blue,
+        ),
+      ),
+      Expanded(
+        child: Container(
+          width: 75,
+          height: 75,
+          color: Colors.red,
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> get flexibleContainer {
+    return [
+      //flexible en fazla verilen genişlik değeri kadar büyür ama gerekirse küçülür.Expanded ise mümkün olan bütün alana yayılır
+      Flexible(
+        child: Container(
+          width: 175,
+          height: 175,
+          color: Colors.yellow,
+        ),
+      ),
+      Flexible(
+        child: Container(
+          width: 175,
+          height: 175,
+          color: Colors.red,
+        ),
+      )
+    ];
   }
 
   Widget containerLessons() {
